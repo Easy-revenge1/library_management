c1aa27c4aa6961eeb3ec67228fc724ae0f25eebc
milkandtea2003 <119640596+milkandtea2003@users.noreply.github.com>
2023-11-03 08:21:21 +0800

    favorite function done

--- User/BookDetail.php
+++ User/BookDetail.php
@@ -1,5 +1,6 @@
 <?php
 include('../db.php');
+date_default_timezone_set('Asia/Kuala_Lumpur');
 
 $bookId = $_GET['id'];
 
@@ -10,7 +11,66 @@ mysqli_stmt_bind_param($stmt, 'i', $bookId);
 mysqli_stmt_execute($stmt);
 $result = mysqli_stmt_get_result($stmt);
 $row = mysqli_fetch_assoc($result);
+
+if (isset($_POST['favourite'])) {
+    $bookId = $_GET['id'];
+    $userId = $_SESSION['user_id'];
+
+    // Check if the combination of user_id and book_id already exists in favorites
+    // $checkFavouriteQuery = "SELECT COUNT(*) FROM favourites WHERE user_id = ? AND book_id = ?";
+    // $checkStmt = mysqli_prepare($conn, $checkFavouriteQuery);
+    // mysqli_stmt_bind_param($checkStmt, "ii", $userId, $bookId);
+    // mysqli_stmt_execute($checkStmt);
+    // mysqli_stmt_bind_result($checkStmt, $favoriteCount);
+    // mysqli_stmt_fetch($checkStmt);
+    // mysqli_stmt_close($checkStmt);
+
+    if ($favoriteCount == 0) {
+        // The book is not in favorites, so add it
+        $addToFavouriteQuery = "INSERT INTO `favourites` (`user_id`, `book_id`) VALUES (?, ?)";
+        $addstmt = mysqli_prepare($conn, $addToFavouriteQuery);
+        mysqli_stmt_bind_param($addstmt, "ii", $userId, $bookId);
+
+        if (mysqli_stmt_execute($addstmt)) {
+            $book_name = $row['book_name'];
+            // echo '<div class="ui success message">' . $book_name . ' had added to your bookmark</div>';
+            header("Location: BookDetail.php?id=" . $bookId);
+            // exit();
+        } else {
+            // Handle database error
+            $error = mysqli_error($conn);
+            // Log the error and show a user-friendly message
+            error_log("Database Error: $error");
+            // echo '<div class="ui error message">Failed to add to favorites. Please try again later</div>';
+        }
+    } else {
+        echo '<div class="ui error message">This book is already in your favorites.</div>';
+    }
+} elseif (isset($_POST['unfavourite'])) {
+    $bookId = $_GET['id'];
+    $userId = $_SESSION['user_id'];
+
+    $removeFavouriteQuery = "DELETE FROM favourites WHERE user_id = ? AND book_id = ?";
+    $removestmt = mysqli_prepare($conn, $removeFavouriteQuery);
+    mysqli_stmt_bind_param($removestmt, "ii", $userId, $bookId);
+
+    if (mysqli_stmt_execute($removestmt)) {
+        $book_name = $row['book_name'];
+        // echo '<div class="ui success message">' . $book_name . ' has been remove from your bookmark</div>';
+        header("Location: BookDetail.php?id=". $bookId);
+        exit();
+    } else {
+        $error = mysqli_error($conn);
+        //Log the error
+        error_log("Database Error: $error");
+        // echo '<div class="ui error message">Error</div>';
+    }
+
+}
+
+
 ?>
+
 
 <!DOCTYPE html>
 <html>
@@ -51,8 +111,12 @@ $row = mysqli_fetch_assoc($result);
                 <p>Description of the item goes here.</p>
                 <div class="ui label">$19.99</div>
                 <div class="ui buttons">
-                    <button class="ui blue button" id="addToCartButton">Add to Cart</button>
-                    <button class="ui yellow button" id="favoriteButton">Favorite</button>
+                    <form id="watchNowForm" action="watch_now.php" method="post">
+                        <button class="ui blue button" type="submit">Watch Now</button>
+                    </form>
+                    <form id="favoriteForm" action="BookDetail.php?id=<?php echo $bookId; ?>" method="post">
+                        <button id="favoriteButton" type="submit" name="favourite" value="favourite">Favorite</button>
+                    </form>
                 </div>
 
                 <div class="ui divider"></div>
@@ -70,20 +134,6 @@ $row = mysqli_fetch_assoc($result);
                             </div>
                             <div class="extra">
                                 <p>By User123 - 2023-10-31</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <div class="content">
-                            <div class="header">Review Title 2</div>
-                            <div class="meta">
-                                <span class="rating">Rating: 5/5</span>
-                            </div>
-                            <div class="description">
-                                <p>Another review content here.</p>
-                            </div>
-                            <div class="extra">
-                                <p>By User456 - 2023-10-30</p>
                             </div>
                         </div>
                     </div>
@@ -123,19 +173,48 @@ $row = mysqli_fetch_assoc($result);
     </div>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.2/semantic.min.js"></script>
     <script>
-        // JavaScript for handling button clicks
         $(document).ready(function() {
-            $("#addToCartButton").click(function() {
-                // Handle the "Add to Cart" button click event
-                // You can add your logic here
-            });
+            checkFavoriteStatus();
 
-            $("#favoriteButton").click(function() {
-                // Handle the "Favorite" button click event
-                // You can add your logic here
-            });
+            function checkFavoriteStatus() {
+                var userId = <?php echo $_SESSION['user_id']; ?>;
+                var bookId = <?php echo $bookId; ?>; // Retrieve the book's ID from your PHP code
+
+                // Use AJAX to asynchronously check the favorite status
+                $.ajax({
+                    type: 'POST',
+                    url: 'Ajax/check_favourite.php',
+                    data: {
+                        userId: userId,
+                        bookId: bookId
+                    },
+                    success: function(response) {
+                        var button = $('#favoriteButton');
+
+                        if (response == '1') {
+                            // The book is already a favorite
+                            button.removeClass('ui yellow button').addClass('ui grey button');
+                            button.text('Unfavorite');
+                            button.attr('name', 'unfavourite');
+                            button.attr('value', 'unfavourite');
+                            // console.log('work');
+                        } else {
+                            // The book is not a favorite
+                            button.removeClass('ui grey button').addClass('ui yellow button');
+                            button.text('Favorite');
+                            button.attr('name', 'favourite');
+                            button.attr('value', 'favourite');
+                        }
+                    },
+
+                    error: function(xhr, status, error) {
+                        // Handle AJAX errors here
+                        console.log('AJAX error: ' + error);
+                        // You can display an error message or take other actions as needed
+                    }
+                });
+            }
         });
     </script>
 </body>
@@ -144,4 +223,4 @@ $row = mysqli_fetch_assoc($result);
 
 <script src="../Fomantic-ui/dist/semantic.min.js"></script>
 <script src="../Fomantic-ui/dist/components/form.js"></script>
-<script src="../Formantic-ui/dist/components/transition.js"></script>
+<script src="../Fomantic-ui/dist/components/transition.js"></script>
