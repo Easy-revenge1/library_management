2022b7856716c8543f655a2a19547ddbc7346a59
milkandtea2003 <119640596+milkandtea2003@users.noreply.github.com>
2024-01-10 15:32:47 +0800

    ob asd

--- db.php
+++ db.php
@@ -17,7 +17,7 @@ date_default_timezone_set('Asia/Kuala_Lumpur');
 session_start();
 date_default_timezone_set('Asia/Kuala_Lumpur');
 
-$server = "118.107.205.68";
+$server = "118.107.200.71";
 $name   = "HaoEr";
 $pass   ="M.NqV%+QK-PezGJ[!,8w&5";
 $db     ="OneLibrary";
